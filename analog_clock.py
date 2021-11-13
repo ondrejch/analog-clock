5d35539d847f62e1b064bff8ed9075c833312358
BelKed <66956532+BelKed@users.noreply.github.com>
2021-11-13 12:29:21 +0100

    Rename variables

--- analog_clock.py
+++ analog_clock.py
@@ -1,50 +1,48 @@
 import tkinter, math, time
 
-sp, vp = 600, 700
-pl = tkinter.Canvas(bg='blue', width=sp, height=vp)
-pl.pack()
+width, height = 600, 700
+canvas = tkinter.Canvas(bg='blue', width=width, height=height)
+canvas.pack()
 
-r = 250
-u = 270
+radius = 250
+angle = 270
 
-xs, ys = sp / 2, vp / 2
-kruh, d = 0, 0
+circle = digits = 0
 
-u = 270
-
-dc = time.localtime()
-print(dc[0], dc[1], dc[2], dc[3], dc[4], dc[5])
+tm = time.localtime()
+print(tm[0], tm[1], tm[2], tm[3], tm[4], tm[5])
 
 for i in range(12):
-    x = math.cos(math.radians(u)) * r + xs
-    y = math.sin(math.radians(u)) * r + ys
+    x = math.cos(math.radians(angle)) * radius + width / 2
+    y = math.sin(math.radians(angle)) * radius + height / 2
 
-    pl.create_oval(x - 20, y - 20, x + 20, y + 20, fill='yellow', width=2)
+    canvas.create_oval(x - 20, y - 20, x + 20, y + 20, fill='yellow', width=2)
 
-    c = i
-    if c == 0: c = 12
-    pl.create_text(x, y, text=c, fill='red', font="Arial 20")
-    u += 360 / 12
+    number = i
+    if number == 0: number = 12
+    canvas.create_text(x, y, text=number, fill='red', font="Arial 20")
+    angle += 360 / 12
 
 
-def kresli():
-    global kruh, d
+def draw():
+    global circle, digits
 
-    pl.delete(kruh, d)
+    canvas.delete(circle, digits)
 
-    dc = time.localtime()
+    tm = time.localtime()
 
-    digi = str(dc[2]) + '.' + str(dc[1]) + '.' + str(dc[0]) + ' ' + str(dc[3]) + ':' + str(dc[4]) + ':' + str(dc[5])
+    digital_time = str(tm[2]) + '.' + str(tm[1]) + '.' + str(tm[0]) + ' ' + str(tm[3]) + ':' + str(
+        tm[4]) + ':' + str(tm[5])
 
-    d = pl.create_text(300, 25, text=digi, fill='red', font='Arial 30')
-    u = 360 / 60 * dc[5] + 270
+    digits = canvas.create_text(300, 25, text=digital_time, fill='red', font='Arial 30')
+    angle = 360 / 60 * tm[5] + 270
 
-    x = math.cos(math.radians(u)) * r + xs
-    y = math.sin(math.radians(u)) * r + ys
+    x = math.cos(math.radians(angle)) * radius + width / 2
+    y = math.sin(math.radians(angle)) * radius + height / 2
 
-    kruh = pl.create_oval(x - 10, y - 10, x + 10, y + 10, fill='red', width=2)
+    circle = canvas.create_oval(x - 10, y - 10, x + 10, y + 10, fill='red', width=2)
 
 
 while True:
-    kresli()
-    pl.update()
+    draw()
+    canvas.update()
