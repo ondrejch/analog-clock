3b13b969ecc6919e4553a406d0f9e6baa90b72ac
BelKed <66956532+BelKed@users.noreply.github.com>
2021-11-13 12:39:12 +0100

    Use `"` instead `'`

--- analog_clock.py
+++ analog_clock.py
@@ -1,7 +1,7 @@
 import tkinter, math, time
 
 width, height = 600, 700
-canvas = tkinter.Canvas(bg='blue', width=width, height=height)
+canvas = tkinter.Canvas(bg="blue", width=width, height=height)
 canvas.pack()
 
 radius = 250
@@ -25,22 +25,22 @@ def draw():
 
     digital_time = f"{tm[2]:02}.{tm[1]:02}.{tm[0]:04} {tm[3]:02}:{tm[4]:02}:{tm[5]:02}"
 
-    digits = canvas.create_text(300, 25, text=digital_time, fill='red', font='Arial 30')
+    digits = canvas.create_text(300, 25, text=digital_time, fill="red", font="Arial 30")
     angle = 360 / 60 * tm[5] + 270
 
     x, y = calculate(angle, radius)
 
-    circle = canvas.create_oval(x - 10, y - 10, x + 10, y + 10, fill='red', width=2)
+    circle = canvas.create_oval(x - 10, y - 10, x + 10, y + 10, fill="red", width=2)
 
 
 for i in range(12):
     x, y = calculate(angle, radius)
 
-    canvas.create_oval(x - 20, y - 20, x + 20, y + 20, fill='yellow', width=2)
+    canvas.create_oval(x - 20, y - 20, x + 20, y + 20, fill="yellow", width=2)
 
     number = i
     if number == 0: number = 12
-    canvas.create_text(x, y, text=number, fill='red', font="Arial 20")
+    canvas.create_text(x, y, text=number, fill="red", font="Arial 20")
     angle += 360 / 12
 
 while True:
