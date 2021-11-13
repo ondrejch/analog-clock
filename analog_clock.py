e2a9d370e22884a26dc98656d75e656efedd3db7
BelKed <66956532+BelKed@users.noreply.github.com>
2021-11-13 18:34:52 +0100

    Add shorter line for each minute

--- analog_clock.py
+++ analog_clock.py
@@ -3,7 +3,7 @@ import math
 import time
 
 canvas_size = 700
-canvas = tkinter.Canvas(bg="#091921", width=canvas_size, height=canvas_size)
+canvas = tkinter.Canvas(bg="#08141a", width=canvas_size, height=canvas_size)
 canvas.pack()
 
 radius = 250
@@ -29,19 +29,19 @@ def draw(elements):
         canvas.delete(element)
 
     tm = time.localtime()
-    
+
     digital_time = time.strftime('%d.%m.%Y %H:%M:%S', tm)
     elements[0] = canvas.create_text(canvas_size / 2, 40, text=digital_time, fill="#804d33",
                                      font=("PT Sans", 25, "bold"))
 
-    x, y = calculate(tm[3] * 30 + tm[4] / 2 - 90, radius - 110)
+    x, y = calculate(tm[3] * 30 + tm[4] / 2 - 90, radius - 90)
     elements[1] = line(x, y, 12, "#591b30")
 
-    x, y = calculate(tm[4] * 6 + tm[5] / 10 - 90, radius - 60)
+    x, y = calculate(tm[4] * 6 + tm[5] / 10 - 90, radius - 40)
     elements[2] = line(x, y, 7, "#1b594f")
 
     x1, y1 = calculate(tm[5] * 6 + 90, radius - 200)
-    x2, y2 = calculate(tm[5] * 6 - 90, radius - 40)
+    x2, y2 = calculate(tm[5] * 6 - 90, radius - 30)
     elements[3] = canvas.create_line(x1, y1, x2, y2, width=4, fill="#1b3f59", capstyle="round")
 
     elements[4] = canvas.create_oval(canvas_size / 2 - 10, canvas_size / 2 - 10,
@@ -52,15 +52,23 @@ def draw(elements):
 
 
 for i in range(12):
-    if (i + 1) % 3 == 0:
-        x, y = calculate(angle + 30, radius)
-        canvas.create_text(x, y, text=i + 1, fill="#336680", font=("PT Sans", 25, "bold"))
-    else:
-        x1, y1 = calculate(angle + 30, radius - 15)
-        x2, y2 = calculate(angle + 30, radius + 15)
-        canvas.create_line(x1, y1, x2, y2, width=5, fill="#1f2b4d", capstyle="round")
+    x, y = calculate(angle + 30, radius - 45)
+    canvas.create_text(x, y, text=i + 1, fill="#336680", font=("PT Sans", 35, "bold"))
 
     angle += 360 / 12
+
+for i in range(60):
+    if i % 5 == 0:
+        x1, y1 = calculate(angle + 30, radius - 10)
+        width = 5
+    else:
+        x1, y1 = calculate(angle + 30, radius)
+        width = 3
+
+    x2, y2 = calculate(angle + 30, radius + 10)
+    canvas.create_line(x1, y1, x2, y2, width=width, fill="#1f2b4d", capstyle="round")
+
+    angle += 360 / 60
 
 while True:
     elements = draw(elements)
