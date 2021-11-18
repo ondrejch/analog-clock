da89a051e0b925d2e6fc7bc9129773692fb5ad5a
BelKed <66956532+BelKed@users.noreply.github.com>
2021-11-18 23:09:51 +0100

    Change fonts

--- analog_clock.py
+++ analog_clock.py
@@ -9,7 +9,7 @@ canvas.pack()
 radius = 250
 angle = 270
 
-elements = [0 for _ in range(5)]
+elements = [0 for _ in range(6)]
 
 
 def calculate(_angle, _radius):
@@ -27,8 +27,9 @@ def line_from_center(_x, _y, _width, _color):
     return line(canvas_size / 2, canvas_size / 2, _x, _y, _width, _color)
 
 
-def text(_x, _y, _text, _color, _font_size):
-    return canvas.create_text(_x, _y, text=_text, fill=_color, font=("PT Sans", _font_size, "bold"))
+def text(_x, _y, _text, _color, _font_size, font="Digital-7 Mono"):
+    return canvas.create_text(_x, _y, text=_text, fill=_color, font=(font, _font_size),
+                              justify="center")
 
 
 def draw(elements):
@@ -38,24 +39,25 @@ def draw(elements):
     tm = time.localtime()
 
     # Digital time
-    digital_time = time.strftime('%d.%m.%Y %H:%M:%S', tm)
-    elements[0] = text(canvas_size / 2, 40, digital_time, "#b36b47", 25)
+    digital_time = time.strftime('%H:%M:%S\n%d.%m.%Y', tm)
+    elements[0] = text(canvas_size / 2, canvas_size / 2 + 80, "88:88:88\n88.88.8888", "#17260f", 40)
+    elements[1] = text(canvas_size / 2, canvas_size / 2 + 80, digital_time, "#7acc52", 40)
 
     # Hour hand
     x, y = calculate(tm[3] * 30 + tm[4] / 2 - 90, radius - 90)
-    elements[1] = line_from_center(x, y, 12, "#8c2a4b")
+    elements[2] = line_from_center(x, y, 12, "#8c2a4b")
 
     # Minute hand
     x, y = calculate(tm[4] * 6 + tm[5] / 10 - 90, radius - 40)
-    elements[2] = line_from_center(x, y, 7, "#2a8c7c")
+    elements[3] = line_from_center(x, y, 7, "#2a8c7c")
 
     # Second hand
     x1, y1 = calculate(tm[5] * 6 + 90, radius - 200)
     x2, y2 = calculate(tm[5] * 6 - 90, radius - 30)
-    elements[3] = line(x1, y1, x2, y2, 4, "#2a638c")
+    elements[4] = line(x1, y1, x2, y2, 4, "#2a638c")
 
     # Small circle in center
-    elements[4] = canvas.create_oval(canvas_size / 2 - 10, canvas_size / 2 - 10,
+    elements[5] = canvas.create_oval(canvas_size / 2 - 10, canvas_size / 2 - 10,
                                      canvas_size / 2 + 10, canvas_size / 2 + 10, fill="#405b80",
                                      width=0)
 
@@ -64,7 +66,7 @@ def draw(elements):
 
 for i in range(12):
     x, y = calculate(angle + 30, radius - 45)
-    text(x, y, i + 1, "#478fb3", 35)
+    text(x, y, i + 1, "#478fb3", 35, font="Roboto")
 
     angle += 360 / 12
 
